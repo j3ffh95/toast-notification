7470331e02f8740ceca5771c6fa4ac9072a290ad
j3ffh95 <j3ffh95@gmail.com>
2020-09-29 10:15:31 -0400

    Created function for the logic

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ const btn = document.querySelector("#btn"),
   container = document.querySelector("#container");
 
 btn.addEventListener("click", createNotification);
+
+function createNotification() {
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+
+  notif.innerText = "Hello there from the World!";
+
+  container.appendChild(notif);
+
+  setTimeout(() => {
+    notif.remove();
+  }, 3000);
+}
